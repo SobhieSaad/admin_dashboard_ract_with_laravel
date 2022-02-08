718684f67fbdfc2c5f8670543c85197fb3700218
Sobhie <sobhie.saad1@gmail.com>
2022-02-08 23:24:05 +0300

    Creat login API with auth

--- laravelreactapi/routes/api.php
+++ laravelreactapi/routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('register',[AuthController::class,'register']);
 
+Route::post('login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
