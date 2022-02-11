2106fd2e81bc29efa35c6aedc4494784e9287b35
Sobhie <sobhie.saad1@gmail.com>
2022-02-11 16:26:32 +0300

    Logout API

--- laravelreactapi/routes/api.php
+++ laravelreactapi/routes/api.php
@@ -18,6 +18,12 @@ Route::post('register',[AuthController::class,'register']);
 
 Route::post('login',[AuthController::class,'login']);
 
+Route::middleware(['auth:sanctum'])->group(function(){
+
+    Route::post('logout',[AuthController::class,'logout']);
+}
+);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- react/reactecom/src/App.js
+++ react/reactecom/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route,Switch} from 'react-router-dom';
+import {BrowserRouter, Redirect, Route,Switch} from 'react-router-dom';
 import MasterLayout from './layouts/admin/MasterLayout';
 import Home from './components/frontend/Home';
 import Login from './components/frontend/auth/Login';
@@ -10,6 +10,11 @@ axios.defaults.baseURL="http://localhost:8000";
 axios.defaults.headers.post['Content-Type']='application/json';
 axios.defaults.headers.post['Accept']='application/json';
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function(config){
+  const token=localStorage.getItem('auth_token');
+  config.headers.Authorization=token? `Bearer ${token}` :'';
+  return config;
+});
 
  function App() {
   return (
@@ -18,10 +23,16 @@ axios.defaults.withCredentials = true;
       <Switch>
             <Route exact path="/" component={Home}/>
             
-            <Route path="/login" component={Login}/>
+            {/* <Route path="/login" component={Login}/>
 
-            <Route path="/register" component={Register}/>
+            <Route path="/register" component={Register}/> */}
 
+            <Route path="/login">
+              {localStorage.getItem('auth_token')? <Redirect to='/'></Redirect>: <Login />}
+            </Route>
+            <Route path="/register">
+              {localStorage.getItem('auth_token')? <Redirect to='/'></Redirect>: <Register />}
+            </Route>
             <Route path="/admin"  name="Admin"  render={(props)=><MasterLayout {...props}/>} />
 
       </Switch>
