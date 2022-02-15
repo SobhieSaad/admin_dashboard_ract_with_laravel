0317cce5fbfa9b6729fd60331974bbf20390b1e0
Sobhie <sobhie.saad1@gmail.com>
2022-02-15 22:50:24 +0300

    Private Route in ReactJS with Authentication for Admin - Axios Interceptor

---
+++ react/reactecom/src/AdminPrivateRoute.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Route,Redirect, useHistory } from "react-router-dom";
+import swal from "sweetalert";
+import MasterLayout from "./layouts/admin/MasterLayout";
+
+function AdminPrivateRoute({...rest}){
+
+    const history=useHistory();
+    const [Authenticated,setAuthenticated] = useState(false);
+
+    const [loading,setLoading] = useState(true);
+
+    useEffect(()=>{
+        axios.get(`api/checkingAuthenticated`).then(res=>{
+            if(res.status===200){
+                setAuthenticated(true);
+            }
+            setLoading(false);
+        });
+
+        return ()=>{
+            setAuthenticated(false);
+        }
+    },[]);
+
+
+    axios.interceptors.response.use(
+        undefined,function axiosRetryInterceptor(err){
+            if(err.response.status ===401){
+                swal("Unauthorized",err.response.data.message,"warning");
+                history.pushState('/');
+            }
+            return Promise.reject(err);
+        }
+    );
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+
+    return(
+
+        <Route {...rest}
+            render={({props,location})=>
+                Authenticated ?
+                (<MasterLayout {...props}/> )  :
+                (<Redirect to={{pathname: '/login',state: {from: location} } } 
+                />)
+        }
+        
+        />
+    );
+}
+
+
+export default AdminPrivateRoute;
