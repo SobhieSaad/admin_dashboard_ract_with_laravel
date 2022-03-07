7bf325423d642151f1156c421fa5ab86b8ad4938
Sobhie <sobhie.saad1@gmail.com>
2022-03-07 23:30:04 +0300

    Integrate PayPal

---
+++ react/reactecom/src/components/frontend/Thankyou.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Thankyou(){
+    return(
+        <div className="card card-body text-center">
+            <div className="px-3">
+                <h5>Thank you for using online payment</h5>
+            </div>    
+        </div>
+    )
+}
+
+export default Thankyou;
